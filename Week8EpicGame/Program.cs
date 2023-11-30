6376a753ac6b842be76201b6b1d0d322e60f978e
robin ellervee <robin.ellervee@gmail.com>
2023-11-30 17:59:17 +0200

    add week8

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,27 @@
+﻿//string[] heroes = { "Potter", "Skywalker", "Croft", "Scooby" };
+//string[] villains = { "Voldemort", "Vader", "Dracula", "Joker", "Thanos" };
+
+string folderPath = @"C:\Users\Robin\OneDrive\Desktop";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+string[] weapon = { "magic wand", "plastic fork", "banana", "sword", "stick" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} saves the day with a {heroWeapon}!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} tries to take over the world with a {villainWeapon}!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rand = new Random();
+    int randomIndex = rand.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿string path = @"C:\Users\robin\OneDrive\Desktop";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] filecontentarray)
+{
+    int i = 1;
+    foreach (string line in filecontentarray)
+    {
+        Console.WriteLine($"{i}. {line}");
+        i++;
+    }
+}
